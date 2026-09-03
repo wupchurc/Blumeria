46aea72b3d45a50ee4c8cc044209c2d30b3266a2
Weston <wupchurc@umn.edu>
2026-09-03 14:26:37 -0500

    initial commit

---
+++ 05-code/snRNA-seq/06_pathway_enrichment_ORA.R
@@ -0,0 +1,13 @@
+# ==============================================================================
+# Script:
+# Purpose: 
+# Input:   
+# Output:  
+# ==============================================================================
+
+library(clusterProfiler)
+library(enrichplot)
+library(org.Rn.eg.db)
+library(DESeq2)
+library(ggplot2)
+library(patchwork)
